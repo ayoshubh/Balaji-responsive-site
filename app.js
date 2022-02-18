20f38bd2ebf9bdb208563ad3c2a14c36b017898b
Atharv Vikhe <atharvvikhe@gmail.com>
2022-02-18 18:08:15 +0530

    Update

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ function showNav() {
 }
 
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 
@@ -108,9 +108,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
